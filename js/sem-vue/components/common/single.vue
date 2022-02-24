3c6785eaacbb56e66d660f3ee416ccd0125e634a
mlmhusdk14 <mlmhusdk14@gmail.com>
2022-02-24 20:09:30 +0900

    vue 환경 세팅 테스트

---
+++ js/sem-vue/components/common/single.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+
+    <h1>
+      외부 vue component를 불러오자
+    </h1>
+    <h1>
+      {{text}}
+    </h1>
+  </div>
+</template>
+
+<script>
+module.exports = {
+  name: 'Page1',
+  props: ['text'],
+  data: () => ({
+  })
+}
+</script>
