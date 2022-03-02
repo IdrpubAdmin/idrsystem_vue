6ce585ed80ecb404915768b6ca89c7ee5820b4b2
mlmhusdk14 <mlmhusdk14@gmail.com>
2022-03-02 17:43:07 +0900

    - footer vue 변환 작업

---
+++ js/sem-vue/components/common/footer_main.vue
@@ -0,0 +1,46 @@
+<template>
+  <!-- main_footer -->
+  <footer id="footer" class="main_footer">
+    <div class="inner">
+
+      <div class="ft_num_wrap">
+        <div class="ft_num">
+          <p class="num_tit"><img src="../images/main/footer_num_1.png" alt="1"></p>
+          <ul class="dot_ul">
+            <li>시스템통합</li>
+            <li>소프트웨어개발</li>
+            <li>정보인프라구축 서비스</li>
+          </ul>
+        </div>
+        <div class="ft_num">
+          <p class="num_tit"><img src="../images/main/footer_num_2.png" alt="2"></p>
+          <ul class="sep_ul">
+            <li>회사명 : (주)아이디알 시스템</li>
+            <li>대표자 : 송돈섭</li>
+            <li class="no_sep">사업자번호 : 211-88-45959</li>
+          </ul>
+        </div>
+        <div class="ft_num">
+          <p class="num_tit"><img src="../images/main/footer_num_3.png" alt="3"></p>
+          <ul class="txt_ul">
+            <li>07205  서울시 영등포구 양평로22길 21 코오롱디지털타워 813호</li>
+            <li>대표번호 : <a href="tel:0269250602">02-6925-0602</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="copyright">
+        <p>Copyrightⓒ2020. IDR Systems Co.,Ltd.All Rights Reserved.</p>
+      </div>
+
+    </div>
+  </footer>
+  <!-- //main_footer -->
+</template>
+
+<script>
+module.exports = {
+  data() {
+
+  },
+}
+</script>
