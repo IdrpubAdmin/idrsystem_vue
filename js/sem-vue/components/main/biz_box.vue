998c985022334477cc6905f462968b9f73a61b00
DESKTOP-6RA6GLP\수지 <seop277@idrsystems.co.kr>
2022-03-03 11:39:04 +0900

    main페이지 v-for 사용하여 변환

---
+++ js/sem-vue/components/main/biz_box.vue
@@ -0,0 +1,67 @@
+<template>
+    <section class="biz_box">
+		<h3 class="sec_tit">IDR 사업영역</h3>
+	    <!-- Swiper -->
+	    <div class="swiper-container">
+		    <div class="swiper-wrapper">
+			    <div class="swiper-slide">
+			    <!-- biz_item -->
+				    <div class="biz_item biz_item_01">
+					    <div class="visual_img">
+						    <img src="../images/main/main_swipe_img_1.png" alt="e-Commerce">
+					    </div>
+				    </div>
+			    </div>
+			    <div class="swiper-slide">
+			    <!-- biz_item -->
+				    <div class="biz_item biz_item_01">
+					    <div class="visual_img">
+						    <img src="../images/main/main_swipe_img_2.png" alt="AI Marketing Curator">
+					    </div>
+				    </div>
+			    </div>
+			    <div class="swiper-slide">
+			    <!-- biz_item -->
+				    <div class="biz_item biz_item_01">
+					    <div class="visual_img">
+						    <img src="../images/main/main_swipe_img_3.png" alt="UI/UX Design">
+					    </div>
+				    </div>
+			    </div>
+		    </div>
+		    <!-- Add Pagination -->
+		    <div class="swiper-pagination"></div>
+	    </div>
+	    <!-- //Swiper -->
+	</section>
+</template>
+
+<script>
+
+module.exports = {
+    data() {
+        return {
+            
+        }
+    },
+    mounted() {
+    const swiper = new Swiper(".swiper-container", {
+        slidesPerView: 1,
+        autoHeight: true,
+        effect: 'fade',
+        fadeEffect: {
+            crossFade: true
+        },
+        autoplay: true,
+	    pagination: {
+            el: '.swiper-pagination',
+	        type: 'bullets',
+            clickable: true,
+            renderBullet: function (index, className) {
+                return '<span class="' + className + '">' + '' + '</span>';
+            },
+        },
+    });
+  },
+}
+</script>
