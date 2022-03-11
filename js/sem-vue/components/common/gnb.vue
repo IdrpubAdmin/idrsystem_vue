f43b7c9694820ecb9c7935f2a79cc8e7b0b73b94
seop2777 <[seop277@idrsystems.co.kr]>
2022-03-11 18:06:23 +0900

    lnb

--- js/sem-vue/components/common/gnb.vue
+++ js/sem-vue/components/common/gnb.vue
@@ -1,26 +1,32 @@
 <template>
   <div class="gnb_wrap on" v-show="gnbModal">
     <ul class="gnb_menu" :class="{ has_wnd_nav : lnbOn }">
-      <li v-for="gnbMenu in list" :key="gnbMenu">
-        <a :href="gnbMenu.href" :data-menu-id="gnbMenu.data"><span>{{gnbMenu.title}}</span></a>
-      </li>
+        <li :class="{on : serviceOn}"><a href="service.html"><span>service</span></a></li>
+				<li :class="{on : worksOn}"><a href="works_2021_01.html"><span>work</span></a></li>
+				<li :class="{on : contactOn}"><a href="contact.html"><span>contact</span></a></li>
+      <!-- <li v-for="gnbMenu in list" :key="gnbMenu">
+        <a :href="gnbMenu.href"><span>{{gnbMenu.title}}</span></a>
+      </li> -->
     </ul>
   </div>
 </template>
 
 <script>
 const reqUrl = window.location.pathname;
-const gnbData = [
-	{class : 'serviceOn', href : 'commerce.html', data : 'gnb_01', title : 'service'},
-	{class : 'workOn', href : 'works_2021_01.html', data : 'gnb_02', title : 'work'},
-	{class : 'contactOn', href : 'intro.html', data : 'gnb_03', title : 'contact'}
-];
+// const gnbData = [
+// 	{class : 'serviceOn', href : 'commerce.html', title : 'service'},
+// 	{class : 'workOn', href : 'works_2021_01.html', title : 'work'},
+// 	{class : 'contactOn', href : 'intro.html', title : 'contact'}
+// ];
 module.exports = {
   data() {
     return {
-      list : gnbData,
+      // list : gnbData,
       gnbModal : false,
       lnbOn : reqUrl.includes('/service') || reqUrl.includes('/contact') || reqUrl.includes('_vue'),
+      serviceOn: reqUrl.includes('/service'),
+      worksOn: reqUrl.includes('/works'),
+      contactOn: reqUrl.includes('/contact')
     }
   },
   created: function() {
--- js/sem-vue/components/service/header_router.vue
+++ js/sem-vue/components/service/header_router.vue
@@ -6,14 +6,13 @@
       </a>
     </h1>
     <ul class="lnb_wrap lnb_d1_wrap" v-if="serviceLnb">
-        <router-link class="d1_li" to="/" tag="li"><a href="#none" data-menu-id=lnb1_01><span>e-commerce</span></a></router-link>
-        <router-link class="d1_li" to="/marketing" tag="li"><a href="#none" data-menu-id=lnb1_02><span>AI-Marketing Curator</span></a></router-link>
-        <router-link class="d1_li" to="/design" tag="li"><a href="#none" data-menu-id=lnb1_03><span>UI/UX Design</span></a></router-link>
+        <router-link v-for="lnbMenu1 in list1" :key="lnbMenu1" class="d1_li" :to="lnbMenu1.to" tag="li"><a href="#none"><span>{{lnbMenu1.title}}</span></a></router-link>
+    </ul>
+    <ul class="lnb_wrap lnb_d1_wrap" v-if="worksLnb">
+        <router-link v-for="lnbMenu2 in list2" :key="lnbMenu2" class="d1_li" :to="lnbMenu2.to" tag="li"><a href="#none"><span>{{lnbMenu2.title}}</span></a></router-link>
     </ul>
     <ul class="lnb_wrap lnb_d1_wrap" v-if="contactLnb">
-        <li class="d1_li"><router-link to="/" data-menu-id=lnb1_01><span>IDR 소개</span></router-link></li>
-        <li class="d1_li"><router-link to="/history" data-menu-id=lnb1_02><span>연혁</span></router-link></li>
-        <li class="d1_li"><router-link to="/recruit" data-menu-id=lnb1_03><span>채용안내</span></router-link></li>
+        <router-link v-for="lnbMenu3 in list3" :key="lnbMenu3" class="d1_li" :to="lnbMenu3.to" tag="li"><a href="#none"><span>{{lnbMenu3.title}}</span></a></router-link>
     </ul>
     <button type="button" class="gnb_btn" title="메뉴버튼" @click="showGnb">
       <span class="line" v-for="line in 3" :key="line"></span>
@@ -23,11 +22,36 @@
 
 <script>
 const reqUrl = window.location.pathname;
+const lnbData1 = [
+  {to : '/', title : 'e-commerce'},
+  {to : '/marketing', title : 'AI-Marketing Curator'},
+  {to : '/design', title : 'UI/UX Design'}
+];
+const lnbData2 = [
+  {to : '/', title : '2021'},
+  {to : '/2020', title : '2020'},
+  {to : '/2019', title : '2019'},
+  {to : '/2018', title : '2018'},
+  {to : '/2017', title : '2017'},
+  {to : '/2016', title : '2016'},
+  {to : '/2015', title : '2015'},
+  {to : '/2014', title : '2014'},
+  {to : '/2013', title : '2013'}
+];
+const lnbData3 = [
+  {to : '/', title : 'IDR 소개'},
+  {to : '/history', title : '연혁'},
+  {to : '/recruit', title : '채용안내'}
+];
 module.exports = {
   data() {
     return {
       serviceLnb: reqUrl.includes('/service'),
+      worksLnb: reqUrl.includes('/works'),
       contactLnb: reqUrl.includes('/contact'),
+      list1 : lnbData1,
+      list2 : lnbData1,
+      list3 : lnbData3,
     }
   },
   methods: {
